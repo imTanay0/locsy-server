f3b58cce0e3c81aeaa8c64af2659af591603672c
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-12-10 01:32:04 +0530

    User model updated

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
 
 const app = express();
 
@@ -16,5 +18,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 // app.use(express.static("public"));
 app.use(cookieParser());
+app.use(morgan("common"));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// Routes
+// app.use("/api/v1/user", userRoutes);
+
+app.get("/", (req, res) => res.send(`Hello Admin`));
 
 export { app };
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import crypto from "crypto-js";
 
 const userSchema = mongoose.Schema(
   {
@@ -55,5 +58,43 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Hash Passowrd before saving
+userSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    try {
+      const hashedPassword = await bcrypt.hash(this.password, 10);
+      this.password = hashedPassword;
+    } catch (error) {
+      throw error;
+    }
+  }
+});
+
+// Generate JWT Token
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+};
+
+// Compare password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Reset Token
+userSchema.methods.getResetToken = async function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.resetPasswordExpire = new Date(Date.now() + 15 * 60 * 1000);
+
+  return resetToken;
+};
 
 export default mongoose.model("User", userSchema);
