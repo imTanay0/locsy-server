7f45d322c112cb6a9a7d8b7db8f234db8fe06fcd
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-12-11 02:55:47 +0530

    update productSchema

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -15,6 +15,10 @@ const productSchema = mongoose.Schema(
       required: true,
       default: 0,
     },
+    offerPrice: {
+      type: Number,
+      default: 0,
+    },
     productImage: {
       public_id: {
         type: String,
--- src/models/SellerModel.js
+++ src/models/SellerModel.js
@@ -1,7 +1,4 @@
 import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import crypto from "crypto-js";
 
 const sellerSchema = mongoose.Schema(
   {
